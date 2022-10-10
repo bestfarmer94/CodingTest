f8c6d7fc5adef03354560a9d63f9ee4fe615cfed
bestfarmer94 <sjasjaruddus@naver.com>
2022-10-10 23:41:22 +0900

    sk+

--- sk/Q3.java
+++ sk/Q3.java
@@ -1,10 +1,18 @@
 package sk;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Q3 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int n = Integer.parseInt(br.readLine());
+		System.out.println(sb);
+		br.close();
 	}
 
 }
