0b6176a5cea16700ac6c627b03e4a36b1df02a3c
bestfarmer94 <sjasjaruddus@naver.com>
2022-10-11 20:26:58 +0900

    sk2

--- sk/Q3.java
+++ sk/Q3.java
@@ -3,6 +3,7 @@ package sk;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Q3 {
 
@@ -11,6 +12,17 @@ public class Q3 {
 		StringBuilder sb = new StringBuilder();
 		
 		int n = Integer.parseInt(br.readLine());
+		int[][] arr = new int[n][n];
+		StringTokenizer st;
+		
+		for(int i=0; i<n; i++) {
+			st = new StringTokenizer(br.readLine());
+			
+			for(int j=0; j<n; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
 		System.out.println(sb);
 		br.close();
 	}
